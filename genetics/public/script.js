4bc70c7c2ae022bcf9a0fe713ed9779637146275
joshr9 <joshruchwarger@gmail.com>
2016-01-29 13:54:10 -0700

    started logic

--- genetics/public/script.js
+++ genetics/public/script.js
@@ -1,1 +1,30 @@
-var app = angular.module("redditClone", []);
+var app = angular.module("genetics", []);
+
+app.contoller('parents', function($scope){
+  $scope.creatures = {
+    redOneEye: "images/redOneEye.png"
+    redTwoEye: "images/redTwoEye.png"
+    redThreeEye: "images/redThreeEye.png"
+    blueOneEye: "images/blueOneEye.png"
+    blueTwoEye: "images/blueTwoEye.png"
+    blueThreeEye: "images/blueThreeEye.png"
+    yellowOneEye: "images/yellowOneEye.png"
+    yellowTwoeye: "images/yellowTwoeye.png"
+    yellowThreeEye: "images/yellowThreeEye.png"
+  }
+  $scope.children = {
+    redOneEye: "images/redOneEye.png"
+    redTwoEye: "images/redTwoEye.png"
+    redThreeEye: "images/redThreeEye.png"
+    blueOneEye: "images/blueOneEye.png"
+    blueTwoEye: "images/blueTwoEye.png"
+    blueThreeEye: "images/blueThreeEye.png"
+    yellowOneEye: "images/yellowOneEye.png"
+    yellowTwoeye: "images/yellowTwoeye.png"
+    yellowThreeEye: "images/yellowThreeEye.png"
+  }
+
+  $scope.getRandomCreature = function(){
+    return Math.floor((Math.random()*10)+1)
+  }
+})
